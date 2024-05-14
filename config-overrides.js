a1692085c5be8f8cfd8a484235c74ca86150cd6b
Tlassarick <72909520+Tlassarick@users.noreply.github.com>
2024-05-14 08:57:46 +0300

    Delete config-overrides.js

--- config-overrides.js
+++
@@ -1,9 +0,0 @@
-const { override, addWebpackConfig } = require('customize-cra');
-
-module.exports = override(
-  addWebpackConfig({
-    devServer: {
-      allowedHosts: ['localhost', 'your.domain.com'],
-    },
-  })
-);
