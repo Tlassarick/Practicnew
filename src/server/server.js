d78389102628662725d33bd7056cadf9e4fe23d3
Tlassarick <72909520+Tlassarick@users.noreply.github.com>
2024-05-15 13:02:00 +0300

    Add files via upload

--- src/server/server.js
+++ src/server/server.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import cors from 'cors';
+import path from 'path';
+
 import { getData,deleteData } from '../database/database.js';
 
 const app = express();
@@ -8,6 +10,7 @@ app.use(express.json());
 app.use(cors({
     origin: 'http://localhost:3000'
 }));
+
 app.get('/', async (req, res) => {
     try {
         const data = await getData();
